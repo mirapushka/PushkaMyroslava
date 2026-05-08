127f44fed434d3f7be9b448d834e85f4bcec81ff
mirapushka <mira77777aa@gmail.com>
2026-05-08 10:43:04 +0200

    .

---
+++ src/hackerrank/TaskStaircase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Практична_2._задача_Hackerrank._Staircase.src.hackerrank
+{
+    internal class TaskStaircase
+    {
+        public static void Solve(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                string line = new string(' ', n - i) + new string('#', i);
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
